da777c96d33528fd2270ce06acb2ec36993085dc
roroarij <rosenarij@gmail.com>
2026-03-19 03:17:59 +0000

    Expose window.gtag for custom analytics events

--- app/layout.js
+++ app/layout.js
@@ -17,9 +17,9 @@ export default function RootLayout({ children }) {
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${measurementId}');
+            window.gtag = function gtag(){window.dataLayer.push(arguments);};
+            window.gtag('js', new Date());
+            window.gtag('config', '${measurementId}');
           `}
         </Script>
         {children}
