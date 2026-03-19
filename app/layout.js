089eaf0520ba4dfffc1bf99fdcaee4a8e28a2c9a
roroarij <rosenarij@gmail.com>
2026-03-19 01:08:36 +0000

    Add GA4 tracking

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,8 @@
+import Script from "next/script";
+
 import "./globals.css";
+
+const measurementId = "G-0FSHK07S04";
 
 export const metadata = {
   title: "Portfolio Backtester",
@@ -8,7 +12,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`} strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${measurementId}');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
